812ed57ea5b1e2150d26fe9e7d272367f13a9ae9
Umuratwa <umuratwar@gmail.com>
2024-05-14 09:43:57 +0200

    capstone project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Home from './components/Home';
+// import Layout from './components/Layout';
+// import Background from '';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Layout />}>
+//           <Route path="/Home" element={<Home />} />
+//         </Route>
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+// App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Layout from './components/Layout';
-
+import About from './components/About'
 function App() {
   return (
     <Router>
+  
       <Routes>
-        <Route path="/" element={<Layout />}>
+      
+        <Route path="/" element={<Layout />} >
           <Route path="/Home" element={<Home />} />
+          <Route path="/About" element={<About />} />
         </Route>
       </Routes>
     </Router>
@@ -16,3 +39,5 @@ function App() {
 }
 
 export default App;
+
+
